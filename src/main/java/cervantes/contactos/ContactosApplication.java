279d5d5784cff88556fda24c4a55f020c710c3ef
Oscar <oscarcr189@outlook.com>
2023-11-04 20:07:52 -0600

    Proyecto contactos1

---
+++ src/main/java/cervantes/contactos/ContactosApplication.java
@@ -0,0 +1,15 @@
+package cervantes.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ContactosApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/cervantes/contactos/ContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package cervantes.contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
