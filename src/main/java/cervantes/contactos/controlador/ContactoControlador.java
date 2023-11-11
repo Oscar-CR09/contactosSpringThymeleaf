e66ee2407435fa116ff4a74ca35a990135665b29
Oscar <oscarcr189@outlook.com>
2023-11-11 17:24:31 -0600

    Proyecto contactactos7,8,9 boton editar y eliminar

--- src/main/java/cervantes/contactos/controlador/ContactoControlador.java
+++ src/main/java/cervantes/contactos/controlador/ContactoControlador.java
@@ -6,8 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,8 +30,36 @@ public class ContactoControlador {
 
         return  "index"; //index.html
 
+    }
+
+    @GetMapping("/agregar")
+    public String mostrarAgregar(){
+        return "agregar";//agregar.html
+    }
+
+    @PostMapping("/agregar")
+    public String agregar(@ModelAttribute("contactoForma") Contacto contacto){
+        logger.info("Contacto agregar: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/"; // se redireige al controlador del path "/"
 
 
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value = "id") int idContacto , ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a editar (mostrar): " + contacto);
+        modelo.put("contacto",contacto);
+        return "editar"; //editar.html
+
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto") Contacto contacto ){
+        logger.info("Contacto a guardar (editar): " +contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";// se redirige al controlar path "/"
 
     }
 }
